0dd7bc7d319a95a7acca797cbe60068f2405b942
odzhan <odzhan@protonmail.com>
2019-03-18 20:12:11 +0000

    upd

--- alpc/ntddk.h
+++ alpc/ntddk.h
@@ -2590,6 +2590,17 @@ typedef enum _PROCESSINFOCLASS {
     MaxProcessInfoClass                             // MaxProcessInfoClass should always be the last enum
 } PROCESSINFOCLASS;
 
+
+typedef struct _THREAD_BASIC_INFORMATION {
+  NTSTATUS                ExitStatus;
+  PVOID                   TebBaseAddress;
+  CLIENT_ID               ClientId;
+  KAFFINITY               AffinityMask;
+  KPRIORITY               Priority;
+  KPRIORITY               BasePriority;
+} THREAD_BASIC_INFORMATION, *PTHREAD_BASIC_INFORMATION;
+
+
 //
 // Thread Information Classes
 //
@@ -2798,6 +2809,209 @@ typedef struct _PEB
 
 } PEB, *PPEB;
 
+typedef struct _RTL_ACTIVATION_CONTEXT_STACK_FRAME *PRTL_ACTIVATION_CONTEXT_STACK_FRAME;
+typedef struct _ACTIVATION_CONTEXT *PACTIVATION_CONTEXT;
+typedef struct _TEB_ACTIVE_FRAME *PTEB_ACTIVE_FRAME;
+typedef struct _TEB_ACTIVE_FRAME_CONTEXT *PTEB_ACTIVE_FRAME_CONTEXT;
+
+typedef struct _RTL_ACTIVATION_CONTEXT_STACK_FRAME {
+     PRTL_ACTIVATION_CONTEXT_STACK_FRAME Previous;
+     PACTIVATION_CONTEXT *ActivationContext;
+     ULONG Flags;
+} RTL_ACTIVATION_CONTEXT_STACK_FRAME, *PRTL_ACTIVATION_CONTEXT_STACK_FRAME;
+
+typedef struct _ACTIVATION_CONTEXT_STACK
+{
+     PRTL_ACTIVATION_CONTEXT_STACK_FRAME ActiveFrame;
+     LIST_ENTRY FrameListCache;
+     ULONG Flags;
+     ULONG NextCookieSequenceNumber;
+     ULONG StackId;
+} ACTIVATION_CONTEXT_STACK, *PACTIVATION_CONTEXT_STACK;
+#define GDI_BATCH_BUFFER_SIZE 310
+
+typedef struct _GDI_TEB_BATCH
+{
+    ULONG Offset;
+    ULONG_PTR HDC;
+    ULONG Buffer[GDI_BATCH_BUFFER_SIZE];
+} GDI_TEB_BATCH, *PGDI_TEB_BATCH;
+
+typedef struct _TEB_ACTIVE_FRAME_CONTEXT
+{
+    ULONG Flags;
+    PSTR FrameName;
+} TEB_ACTIVE_FRAME_CONTEXT, *PTEB_ACTIVE_FRAME_CONTEXT;
+
+typedef struct _TEB_ACTIVE_FRAME
+{
+    ULONG Flags;
+    struct _TEB_ACTIVE_FRAME *Previous;
+    PTEB_ACTIVE_FRAME_CONTEXT Context;
+} TEB_ACTIVE_FRAME, *PTEB_ACTIVE_FRAME;
+
+typedef struct _TEB
+{
+    NT_TIB NtTib;
+
+    PVOID EnvironmentPointer;
+    CLIENT_ID ClientId;
+    PVOID ActiveRpcHandle;
+    PVOID ThreadLocalStoragePointer;
+    PPEB ProcessEnvironmentBlock;
+
+    ULONG LastErrorValue;
+    ULONG CountOfOwnedCriticalSections;
+    PVOID CsrClientThread;
+    PVOID Win32ThreadInfo;
+    ULONG User32Reserved[26];
+    ULONG UserReserved[5];
+    PVOID WOW32Reserved;
+    LCID CurrentLocale;
+    ULONG FpSoftwareStatusRegister;
+    PVOID SystemReserved1[54];
+    NTSTATUS ExceptionCode;
+    PVOID ActivationContextStackPointer;
+#ifdef _M_X64
+    UCHAR SpareBytes[24];
+#else
+    UCHAR SpareBytes[36];
+#endif
+    ULONG TxFsContext;
+
+    GDI_TEB_BATCH GdiTebBatch;
+    CLIENT_ID RealClientId;
+    HANDLE GdiCachedProcessHandle;
+    ULONG GdiClientPID;
+    ULONG GdiClientTID;
+    PVOID GdiThreadLocalInfo;
+    ULONG_PTR Win32ClientInfo[62];
+    PVOID glDispatchTable[233];
+    ULONG_PTR glReserved1[29];
+    PVOID glReserved2;
+    PVOID glSectionInfo;
+    PVOID glSection;
+    PVOID glTable;
+    PVOID glCurrentRC;
+    PVOID glContext;
+
+    NTSTATUS LastStatusValue;
+    UNICODE_STRING StaticUnicodeString;
+    WCHAR StaticUnicodeBuffer[261];
+
+    PVOID DeallocationStack;
+    PVOID TlsSlots[64];
+    LIST_ENTRY TlsLinks;
+
+    PVOID Vdm;
+    PVOID ReservedForNtRpc;
+    PVOID DbgSsReserved[2];
+
+    ULONG HardErrorMode;
+#ifdef _M_X64
+    PVOID Instrumentation[11];
+#else
+    PVOID Instrumentation[9];
+#endif
+    GUID ActivityId;
+
+    PVOID SubProcessTag;
+    PVOID EtwLocalData;
+    PVOID EtwTraceData;
+    PVOID WinSockData;
+    ULONG GdiBatchCount;
+
+    union
+    {
+        PROCESSOR_NUMBER CurrentIdealProcessor;
+        ULONG IdealProcessorValue;
+        struct
+        {
+            UCHAR ReservedPad0;
+            UCHAR ReservedPad1;
+            UCHAR ReservedPad2;
+            UCHAR IdealProcessor;
+        };
+    };
+
+    ULONG GuaranteedStackBytes;
+    PVOID ReservedForPerf;
+    PVOID ReservedForOle;
+    ULONG WaitingOnLoaderLock;
+    PVOID SavedPriorityState;
+    ULONG_PTR SoftPatchPtr1;
+    PVOID ThreadPoolData;
+    PVOID *TlsExpansionSlots;
+#ifdef _M_X64
+    PVOID DeallocationBStore;
+    PVOID BStoreLimit;
+#endif
+    ULONG MuiGeneration;
+    ULONG IsImpersonating;
+    PVOID NlsCache;
+    PVOID pShimData;
+    ULONG HeapVirtualAffinity;
+    HANDLE CurrentTransactionHandle;
+    PTEB_ACTIVE_FRAME ActiveFrame;
+    PVOID FlsData;
+
+    PVOID PreferredLanguages;
+    PVOID UserPrefLanguages;
+    PVOID MergedPrefLanguages;
+    ULONG MuiImpersonation;
+
+    union
+    {
+        USHORT CrossTebFlags;
+        USHORT SpareCrossTebBits : 16;
+    };
+    union
+    {
+        USHORT SameTebFlags;
+        struct
+        {
+            USHORT SafeThunkCall : 1;
+            USHORT InDebugPrint : 1;
+            USHORT HasFiberData : 1;
+            USHORT SkipThreadAttach : 1;
+            USHORT WerInShipAssertCode : 1;
+            USHORT RanProcessInit : 1;
+            USHORT ClonedThread : 1;
+            USHORT SuppressDebugMsg : 1;
+            USHORT DisableUserStackWalk : 1;
+            USHORT RtlExceptionAttached : 1;
+            USHORT InitialThread : 1;
+            USHORT SessionAware : 1;
+            USHORT SpareSameTebBits : 4;
+        };
+    };
+
+    PVOID TxnScopeEnterCallback;
+    PVOID TxnScopeExitCallback;
+    PVOID TxnScopeContext;
+    ULONG LockCount;
+    ULONG SpareUlong0;
+    PVOID ResourceRetValue;
+    PVOID ReservedForWdf;
+} TEB, *PTEB;
+
+/**
+typedef struct _GDI_TEB_BATCH {
+     ULONG Offset;
+     ULONG HDC;
+     ULONG Buffer[310];
+} GDI_TEB_BATCH, *PGDI_TEB_BATCH;
+
+typedef struct _TEB_ACTIVE_FRAME {
+     ULONG Flags;
+     PTEB_ACTIVE_FRAME Previous;
+     PTEB_ACTIVE_FRAME_CONTEXT Context;
+} TEB_ACTIVE_FRAME, *PTEB_ACTIVE_FRAME;
+
+typedef struct _TEB_ACTIVE_FRAME_CONTEXT {
+     ULONG Flags;
+     CHAR * FrameName;
+} TEB_ACTIVE_FRAME_CONTEXT, *PTEB_ACTIVE_FRAME_CONTEXT;
 
 //
 // Thread environment block
@@ -2805,21 +3019,105 @@ typedef struct _PEB
 
 typedef struct _TEB
 {
-    NT_TIB NtTib;
-    PVOID  EnvironmentPointer;
-    CLIENT_ID ClientId;
-    PVOID ActiveRpcHandle;
-    PVOID ThreadLocalStoragePointer;
-    PPEB ProcessEnvironmentBlock;
-    ULONG LastErrorValue;
-    ULONG CountOfOwnedCriticalSections;
-    PVOID CsrClientThread;
-    PVOID Win32ThreadInfo;
-    // Incomplete
-
+     NT_TIB NtTib;
+     PVOID EnvironmentPointer;
+     CLIENT_ID ClientId;
+     PVOID ActiveRpcHandle;
+     PVOID ThreadLocalStoragePointer;
+     PPEB ProcessEnvironmentBlock;
+     ULONG LastErrorValue;
+     ULONG CountOfOwnedCriticalSections;
+     PVOID CsrClientThread;
+     PVOID Win32ThreadInfo;
+     ULONG User32Reserved[26];
+     ULONG UserReserved[5];
+     PVOID WOW32Reserved;
+     ULONG CurrentLocale;
+     ULONG FpSoftwareStatusRegister;
+     VOID * SystemReserved1[54];
+     LONG ExceptionCode;
+     PACTIVATION_CONTEXT_STACK ActivationContextStackPointer;
+     UCHAR SpareBytes1[36];
+     ULONG TxFsContext;
+     GDI_TEB_BATCH GdiTebBatch;
+     CLIENT_ID RealClientId;
+     PVOID GdiCachedProcessHandle;
+     ULONG GdiClientPID;
+     ULONG GdiClientTID;
+     PVOID GdiThreadLocalInfo;
+     ULONG Win32ClientInfo[62];
+     VOID * glDispatchTable[233];
+     ULONG glReserved1[29];
+     PVOID glReserved2;
+     PVOID glSectionInfo;
+     PVOID glSection;
+     PVOID glTable;
+     PVOID glCurrentRC;
+     PVOID glContext;
+     ULONG LastStatusValue;
+     UNICODE_STRING StaticUnicodeString;
+     WCHAR StaticUnicodeBuffer[261];
+     PVOID DeallocationStack;
+     VOID * TlsSlots[64];
+     LIST_ENTRY TlsLinks;
+     PVOID Vdm;
+     PVOID ReservedForNtRpc;
+     VOID * DbgSsReserved[2];
+     ULONG HardErrorMode;
+     VOID * Instrumentation[9];
+     GUID ActivityId;
+     PVOID SubProcessTag;
+     PVOID EtwLocalData;
+     PVOID EtwTraceData;
+     PVOID WinSockData;
+     ULONG GdiBatchCount;
+     UCHAR SpareBool0;
+     UCHAR SpareBool1;
+     UCHAR SpareBool2;
+     UCHAR IdealProcessor;
+     ULONG GuaranteedStackBytes;
+     PVOID ReservedForPerf;
+     PVOID ReservedForOle;
+     ULONG WaitingOnLoaderLock;
+     PVOID SavedPriorityState;
+     ULONG SoftPatchPtr1;
+     PVOID ThreadPoolData;
+     VOID * * TlsExpansionSlots;
+     ULONG ImpersonationLocale;
+     ULONG IsImpersonating;
+     PVOID NlsCache;
+     PVOID pShimData;
+     ULONG HeapVirtualAffinity;
+     PVOID CurrentTransactionHandle;
+     PTEB_ACTIVE_FRAME ActiveFrame;
+     PVOID FlsData;
+     PVOID PreferredLanguages;
+     PVOID UserPrefLanguages;
+     PVOID MergedPrefLanguages;
+     ULONG MuiImpersonation;
+     WORD CrossTebFlags;
+     ULONG SpareCrossTebBits: 16;
+     WORD SameTebFlags;
+     ULONG DbgSafeThunkCall: 1;
+     ULONG DbgInDebugPrint: 1;
+     ULONG DbgHasFiberData: 1;
+     ULONG DbgSkipThreadAttach: 1;
+     ULONG DbgWerInShipAssertCode: 1;
+     ULONG DbgRanProcessInit: 1;
+     ULONG DbgClonedThread: 1;
+     ULONG DbgSuppressDebugMsg: 1;
+     ULONG SpareSameTebBits: 8;
+     PVOID TxnScopeEnterCallback;
+     PVOID TxnScopeExitCallback;
+     PVOID TxnScopeContext;
+     ULONG LockCount;
+     ULONG ProcessRundown;
+     UINT64 LastSwitchTime;
+     UINT64 TotalSwitchOutTime;
+     LARGE_INTEGER WaitReasonBitMap;
 } TEB, *PTEB;
 
-
+*/
 typedef struct _PROCESS_BASIC_INFORMATION
 {
     NTSTATUS ExitStatus;
